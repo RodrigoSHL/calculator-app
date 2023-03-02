fb8014f65ac7185e93563de3224a61ce5f3afd25
“RodrigoSHL” <shl.catalan@gmail.com>
2023-03-01 23:43:16 -0300

    FEAT: create calculator view

--- src/screens/CalculatorScreen.tsx
+++ src/screens/CalculatorScreen.tsx
@@ -1,13 +1,43 @@
-import { Text, View } from 'react-native'
-import React from 'react'
-import { styles } from '../../src/theme/appTheme';
+import {Text, View} from 'react-native';
+import React from 'react';
+import {styles} from '../../src/theme/appTheme';
+import {ButtonCalc} from '../components/ButtonCalc';
+
+
 
 const CalculatorScreen = () => {
   return (
-    <View>
-      <Text style={styles.text}>CalculatorScreen</Text>
-    </View>
-  )
-}
+    <View style={styles.calculatorContainer}>
+      <Text style={styles.smallResult}>1,400.000</Text>
+      <Text style={styles.result}>1,400.000</Text>
 
-export default CalculatorScreen
+      <View style={styles.buttonRow}>
+        <ButtonCalc text={'C'} color={'#9b9b9b'}/>
+        <ButtonCalc text={'+/-'} color={'#9b9b9b'}/>
+        <ButtonCalc text={'del'} color={'#9b9b9b'}/>
+        <ButtonCalc text={'/'} color={'#ff9427'}/>
+      </View>
+      <View style={styles.buttonRow}>
+        <ButtonCalc text={'1'} />
+        <ButtonCalc text={'2'} />
+        <ButtonCalc text={'3'}  />
+        <ButtonCalc text={'*'} color={'#ff9427'} />
+      </View>
+      <View style={styles.buttonRow}>
+        <ButtonCalc text={'4'} />
+        <ButtonCalc text={'5'} />
+        <ButtonCalc text={'6'}  />
+        <ButtonCalc text={'+'} color={'#ff9427'}/>
+      </View>
+      <View style={styles.buttonRow}>
+        <ButtonCalc text={'7'} />
+        <ButtonCalc text={'8'} />
+        <ButtonCalc text={'9'}  />
+        <ButtonCalc text={'-'} color={'#ff9427'} />
+      </View>
+      
+    </View>
+  );
+};
+
+export default CalculatorScreen;
