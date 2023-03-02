2f9135c01b24a934d775ed9a004ed1fd541687d7
“RodrigoSHL” <shl.catalan@gmail.com>
2023-03-01 23:42:37 -0300

    FEAT: create app css theme

--- src/theme/appTheme.tsx
+++ src/theme/appTheme.tsx
@@ -1,12 +1,44 @@
-import { StyleSheet } from "react-native";
+import {StyleSheet} from 'react-native';
 
 export const styles = StyleSheet.create({
-    backgroundBase: {
-        backgroundColor: 'black',
-        flex: 1
-    },
-    text: {
-        fontSize: 20,
-        color: 'white'
-    }
+  backgroundBase: {
+    backgroundColor: 'black',
+    flex: 1,
+  },
+  calculatorContainer: {
+    flex: 1,
+    paddingHorizontal: 20,
+    backgroundColor: 'black',
+    justifyContent: 'flex-end',
+  },
+  result: {
+    fontSize: 60,
+    color: 'white',
+    textAlign: 'right',
+  },
+  smallResult: {
+    fontSize: 30,
+    color: 'rgba(255,255,255,0.5)',
+    textAlign: 'right',
+  },
+  buttonRow:{
+    flexDirection: 'row',
+    justifyContent: 'center',
+    marginBottom: 18,
+    paddingHorizontal: 10
+  },
+  button: {
+    backgroundColor: '#d1d7db',
+    width: 70,
+    height: 70,
+    borderRadius: 100,
+    justifyContent: 'center',
+  },
+  textButton: {
+    textAlign: 'center',
+    padding: 10,
+    fontSize: 30,
+    fontWeight: '300',
+    color: 'black'
+  },
 });
