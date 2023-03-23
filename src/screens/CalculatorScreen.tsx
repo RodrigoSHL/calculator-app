e1519f1db739590f7953abd2732dbfff89f505c3
RodrigoSHL <shl.catalan@gmail.com>
2023-03-23 00:08:22 -0300

    FEAT: implement add, subtract and multiply

--- src/screens/CalculatorScreen.tsx
+++ src/screens/CalculatorScreen.tsx
@@ -1,11 +1,20 @@
 import {Text, View} from 'react-native';
-import React, {useState} from 'react';
+import React, {useState, useRef} from 'react';
 import {styles} from '../../src/theme/appTheme';
 import {ButtonCalc} from '../components/ButtonCalc';
+
+enum Operators {
+  add,
+  subtract,
+  divide,
+  multiply,
+}
 
 const CalculatorScreen = () => {
   const [number, setNumber] = useState('0');
   const [prevNumber, setPrevNumber] = useState('0');
+
+  const lastOperation = useRef<Operators>();
 
   const cleanResult = () => {
     setNumber('0');
@@ -56,6 +65,57 @@ const CalculatorScreen = () => {
     }
   };
 
+  const changeNumberByPrev = () => {
+    if (number.endsWith('.')) {
+      setPrevNumber(number.slice(0, -1));
+    } else {
+      setPrevNumber(number);
+    }
+    setNumber('0');
+  };
+
+  const btnDivide = () => {
+    changeNumberByPrev();
+    lastOperation.current = Operators.divide;
+  };
+
+  const btnMultiply = () => {
+    changeNumberByPrev();
+    lastOperation.current = Operators.multiply;
+  };
+
+  const btnAdd = () => {
+    changeNumberByPrev();
+    lastOperation.current = Operators.add;
+  };
+
+  const btnSubtract = () => {
+    changeNumberByPrev();
+    lastOperation.current = Operators.subtract;
+  };
+
+  const calcule = () => {
+    const num1 = Number(number);
+    const num2 = Number(prevNumber);
+
+    switch (lastOperation.current) {
+      case Operators.add:
+        setNumber(`${num1 + num2}`);
+        break;
+      case Operators.subtract:
+        setNumber(`${num1 - num2}`);
+        break;
+      case Operators.multiply:
+        setNumber(`${num1 * num2}`);
+        break;
+      case Operators.divide:
+        setNumber(`${num1 / num2}`);
+        break;
+      default:
+        break;
+    }
+  };
+
   return (
     <View style={styles.calculatorContainer}>
       <Text style={styles.smallResult}>{prevNumber}</Text>
@@ -65,34 +125,34 @@ const CalculatorScreen = () => {
         <ButtonCalc text={'C'} color={'#9b9b9b'} action={cleanResult} />
         <ButtonCalc text={'+/-'} color={'#9b9b9b'} action={positiveNegative} />
         <ButtonCalc text={'del'} color={'#9b9b9b'} action={deleteNumber} />
-        <ButtonCalc text={'/'} color={'#ff9427'} action={cleanResult} />
+        <ButtonCalc text={'/'} color={'#ff9427'} action={btnDivide} />
       </View>
 
       <View style={styles.buttonRow}>
         <ButtonCalc text={'1'} action={buildNumber} />
         <ButtonCalc text={'2'} action={buildNumber} />
         <ButtonCalc text={'3'} action={buildNumber} />
-        <ButtonCalc text={'*'} color={'#ff9427'} action={cleanResult} />
+        <ButtonCalc text={'*'} color={'#ff9427'} action={btnMultiply} />
       </View>
 
       <View style={styles.buttonRow}>
         <ButtonCalc text={'4'} action={buildNumber} />
         <ButtonCalc text={'5'} action={buildNumber} />
         <ButtonCalc text={'6'} action={buildNumber} />
-        <ButtonCalc text={'+'} color={'#ff9427'} action={cleanResult} />
+        <ButtonCalc text={'+'} color={'#ff9427'} action={btnAdd} />
       </View>
 
       <View style={styles.buttonRow}>
         <ButtonCalc text={'7'} action={buildNumber} />
         <ButtonCalc text={'8'} action={buildNumber} />
         <ButtonCalc text={'9'} action={buildNumber} />
-        <ButtonCalc text={'-'} color={'#ff9427'} action={cleanResult} />
+        <ButtonCalc text={'-'} color={'#ff9427'} action={btnSubtract} />
       </View>
 
       <View style={styles.buttonRow}>
         <ButtonCalc text={'0'} buttonWidth action={buildNumber} />
         <ButtonCalc text={'.'} action={buildNumber} />
-        <ButtonCalc text={'='} color={'#ff9427'} action={cleanResult} />
+        <ButtonCalc text={'='} color={'#ff9427'} action={calcule} />
       </View>
     </View>
   );
